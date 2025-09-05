6e793423bf61798fe55671f1c767cca3c568d155
Lucas <jrsharpie@outlook.com>
2025-09-05 13:34:48 +1000

    test OutputIO

--- Gat-IJ-Plugin/GAT-Java-Plugin/src/test/java/FeatureTests/Tools/uniqueDirTest.java
+++ Gat-IJ-Plugin/GAT-Java-Plugin/src/test/java/FeatureTests/Tools/uniqueDirTest.java
@@ -9,9 +9,9 @@ import java.io.File;
 public class uniqueDirTest {
     @Test
     public void testUniqueDir() {
-        File baseDir = new File("C:/Users/jrsha/Software Projects/Monash/GAT/_Analyse Neuron (Multi-channel - No Hu).ijm");
+        File baseDir = new File("C:/Users/jrsha/Software Projects/Monash/GAT/Gat-IJ-Plugin/GAT-Java-Plugin/src/test/java/FeatureTests/Tools/uniqueDirTest.java");
         File uniqueDir1 = OutputIO.uniqueDir(baseDir);
 //        File uniqueDir2 = OutputIO.uniqueDir(baseDir);
-        assertEquals("_Analyse Neuron (Multi-channel - No Hu)_1", uniqueDir1.getName(), "unique directory should be" + uniqueDir1.getName());
+        assertEquals("uniqueDirTest_1.java", uniqueDir1.getName(), "unique directory should be" + uniqueDir1.getName());
     }
 }
